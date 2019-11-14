727399cb81f7192bba86112b134d68c557b6a68d
Liza <shotsbyliza@gmail.com>
2019-11-14 17:34:28 -0500

    fix?

---
+++ e17/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  console.log('why is this not working?');
+});
+
+$('.margie').click(function(){
+  $('.number-1').css('text-align','left')
+  alert("The paragraph was clicked.");
+});
