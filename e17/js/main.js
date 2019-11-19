04aff0f80731c4d95a847efa32fe78055f20587c
Liza <shotsbyliza@gmail.com>
2019-11-19 15:06:05 -0500

    alldoneyay

--- e17/js/main.js
+++ e17/js/main.js
@@ -3,6 +3,17 @@ $(document).ready(function(){
 });
 
 $('.margie').click(function(){
-  $('.number-1').css('text-align','left')
-  alert("The paragraph was clicked.");
+  $('.number-1').css('text-align','left');
+});
+$('.mojito').dblclick(function(){
+  $('.number-2').after("<p>Extra Mint Please!<p>");
+});
+$('.gin').hover(function(){
+  $('.number-3').css('text-align','right');
+});
+$('.ginger').click(function(){
+  $('.number-4').css("color", "gold");
+});
+$('.sangria').hover(function(){
+  $('.number-5').css("color", "maroon");
 });
